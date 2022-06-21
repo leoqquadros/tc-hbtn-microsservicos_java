1b8d5e8c28bf26227a752cd0d9cd3f0bd5033cf8
Leonardo Quadros <leonardoq@ciandt.com>
2022-06-21 11:43:41 -0300

    Microsserviços

---
+++ 0x01/spring_boot_1/com/example/demo/MessageResource.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/messages")
+public class MessageResource {
+    @GetMapping("/simpleMessageWelcome")
+    public String simpleMessageWelcome() {
+        return "BEM VINDO A AULA DE MICROSSERVIÇO USANDO SPRING BOOT !!!";
+    }
+
+    @GetMapping("/login")
+    public String login(@RequestParam String user, @RequestParam String password) {
+
+        if (user.equals("") || password.equals(""))
+            return "USUÁRIO E SENHA NÃO INFORMADOS";
+
+        else if (user.length() > 15 || password.length() > 15) {
+            return "USUÁRIO E SENHA INVÁLIDOS";
+        }
+
+        else {
+            return "LOGIN EFETUADO COM SUCESSO !!!";
+        }
+
+
+
+    }
+}
