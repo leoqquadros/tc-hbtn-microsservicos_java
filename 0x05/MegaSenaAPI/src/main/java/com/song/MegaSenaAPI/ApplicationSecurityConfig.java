3828eddf3241737bc53e2fec3cf64629a348af71
Leonardo Quadros <leonardoq@ciandt.com>
2022-06-28 12:04:51 -0300

    Microsserviços

---
+++ 0x05/MegaSenaAPI/src/main/java/com/song/MegaSenaAPI/ApplicationSecurityConfig.java
@@ -0,0 +1,24 @@
+package com.song.MegaSenaAPI;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET,"/megasena/simpleMessageWelcome").permitAll()
+                .anyRequest()
+                .authenticated();
+
+    }
+
+}
---
+++ 0x05/MegaSenaAPI/src/main/java/com/song/MegaSenaAPI/MegaSenaController.java
@@ -0,0 +1,33 @@
+package com.song.MegaSenaAPI;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.swing.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+
+
+
+@RestController
+@RequestMapping(value="/megasena")
+public class MegaSenaController {
+
+    @GetMapping("/simpleMessageWelcome")
+    public String mensagemBoasVindas() {
+
+        return "Geração de números da Mega-Sena";
+
+    }
+
+
+    @GetMapping("/getNumbers")
+    public List<Integer> numerosMegaSena() {
+        Random gera = new Random();
+        return gera.ints(10).boxed().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+
+    }
+
+}
